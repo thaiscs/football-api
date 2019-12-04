d8c480946b3f4eedabeb20b838991aca3d724d86
thaiscs <thais.advj@gmail.com>
2019-12-04 17:07:29 +0100

    added router

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 4000;
-const database = require("./database");
-const team = require("./team/model");
+const teamRouter = require("./team/router");
 
+app.use(teamRouter); // To register the Middleware
 app.get("/", (request, response) => response.send("Football API"));
 app.listen(4000, () => console.log(`Football API listening on ${port}`));
