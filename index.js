fa4ce386768c024fe6c0c4451fd95286e0b69a9e
thaiscs <thais.advj@gmail.com>
2019-12-04 14:46:28 +0100

    start of API

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.get("/", (request, response) => response.send("Football API"));
+app.listen(4000, () => console.log(`Football API listening on ${port}`));
